538621d9c565d0f9d898e4c5324c3dd5e157c031
Solomon <somuruka@gmail.com.com>
2023-03-23 10:03:27 -0700

    positive values are sometimes better that negetivr values

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Determine if a randon number is positive, negetive or zero.
+*
+* Return: 0 on success
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+        if (n < 0)
+	{	
+              printf("%d is %s\n", n, "negetive");
+        }
+        else if (n > 0)
+        {
+             printf("%d is %s\n", n, " positive");
+        {
+        else 
+	} 
+            printf("%d is %s\n", n, "zero");
+	}
+	return (0);
